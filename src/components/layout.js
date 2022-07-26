d1d44fb6bd766a8c35324f6d212228b2eb20733e
pieh <misiek.piechowiak@gmail.com>
2022-07-26 19:20:32 +0200

    Add link to GraphiQL with example query using sort and aggregation fields on each page

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,6 +23,12 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
+      <a
+        className="link-graphiql"
+        href="/___graphql?query=%7B%0A%20%20allMarkdownRemark(sort%3A%20%7Bfrontmatter%3A%20%7Bdate%3A%20DESC%7D%7D)%20%7B%0A%20%20%20%20nodes%20%7B%0A%20%20%20%20%20%20fields%20%7B%0A%20%20%20%20%20%20%20%20slug%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20frontmatter%20%7B%0A%20%20%20%20%20%20%20%20date(formatString%3A%20%22MMMM%20DD%2C%20YYYY%22)%0A%20%20%20%20%20%20%20%20title%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%20%20totalTimeToRead%3A%20sum(field%3A%20%7BtimeToRead%3A%20SELECT%7D)%0A%20%20%20%20group(field%3A%20%7BtimeToRead%3A%20SELECT%7D)%20%7B%0A%20%20%20%20%20%20field%0A%20%20%20%20%20%20fieldValue%0A%20%20%20%20%20%20nodes%20%7B%0A%20%20%20%20%20%20%20%20frontmatter%20%7B%0A%20%20%20%20%20%20%20%20%20%20title%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A"
+      >
+        Visit GraphiQL to explore new API
+      </a>
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
